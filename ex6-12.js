c678de38d7a1f38b9648cf6d61ecb48d12759232
numazakisara200525 <167832970+numazakisara200525@users.noreply.github.com>
2024-12-03 10:14:59 +0900

    Update ex6-12.js

--- ex6-12.js
+++ ex6-12.js
@@ -1,7 +1,7 @@
 function hyoji13() {
 var str13 = "ようこそ";
 var cnt13 = document.timer13.moji13.value.length;
-if ( cnt13 < 11 ) {
+if ( cnt13 < 4 ) {
 document.timer13.moji13.value = str13.substr(0,cnt13+1); } else {
 document.timer13.moji13.value = ""; }
 setTimeout("hyoji13()",500); }
