d27883d0f3f3a1160456e65b1723a1b808cdd1bc
numazakisara200525 <167832970+numazakisara200525@users.noreply.github.com>
2024-12-03 10:04:28 +0900

    Create ex6-12.js

---
+++ ex6-12.js
@@ -0,0 +1,7 @@
+function hyoji() {
+var str = "ようこそ"; 
+var cnt = document.timer.moji.value.length;
+if ( cnt < 11 ) {document.timer.moji.value = str.substr(0,cnt+1); }
+else {document.timer.moji.value = ""; } }
+function startfnc() {
+setInterval("hyoji()",1000); }
