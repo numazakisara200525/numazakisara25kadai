763a57465adbd7c3d4c5ed05c73fcaad14d8c5c9
numazakisara200525 <167832970+numazakisara200525@users.noreply.github.com>
2025-01-07 09:37:41 +0900

    Update ex1-4.js

--- ex1-4.js
+++ ex1-4.js
@@ -1,3 +1,3 @@
 function imgchange1() {
-document.getElementById('urasima').src = './image/5maime.png';
-}
+document.getElementById('urasima').src = './image/5maime.png';}
+function imgchange2() {document.getElementById('urasima').src = './image/4maime.png';}
